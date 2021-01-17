0f17a6d71f2c8e32529b507ba53ef08a3170e711
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-01-17 12:19:52 +0100

    vytvorena kostra podla dizajnu

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'project';
+  newServerName = '';
+  newServerContent = '';
+
+  serverElements = [{ type: 'red', name: 'test', content: 'test' }];
+
+  onAddRedServers() {
+    this.serverElements.push({
+      type: 'red',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+  onAddBlueServers() {
+    this.serverElements.push({
+      type: 'blue',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
 }
