3a1ce2f64cb09a44fc11e103f4359f6421f7fdd7
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-01-21 13:31:06 +0100

    @Input v server-element.component.ts

--- project/src/app/server-element/server-element.component.ts
+++ project/src/app/server-element/server-element.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.scss'],
 })
-export class ServerElementComponent {}
+export class ServerElementComponent {
+  @Input() serverElement: { type: string; name: string; content: string };
+}
