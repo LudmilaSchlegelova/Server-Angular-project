440f12aea95b024346b1d414cbdbe6ec32a1e74a
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-01-21 13:20:35 +0100

    @Output, EventEmitter(), emit() v cockpit.components.ts

--- project/src/app/cockpit/cockpit.component.ts
+++ project/src/app/cockpit/cockpit.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.scss'],
 })
-export class CockpitComponent {}
+export class CockpitComponent {
+  serverName = '';
+  serverContent = '';
+
+  //Kedze mame dve funkcie, ktore po kliknuti na button maju prepisovat stav v premennych, musime si vytvorit instanciu EventEmitter(). Ta sa pouziva na vytvorenie vazby funkcie s udalostou, emit sa pouziva na spustenie udalosti. Vyuzivame @Output dekorator, kedze data posielame smerom von z komponenty
+  @Output() createGreenServers = new EventEmitter<{
+    name: string;
+    content: string;
+  }>();
+  @Output() createYellowServers = new EventEmitter<{
+    name: string;
+    content: string;
+  }>();
+
+  addGreenServers() {
+    this.createGreenServers.emit({
+      name: this.serverName,
+      content: this.serverContent,
+    });
+  }
+  addYellowServers() {
+    this.createYellowServers.emit({
+      name: this.serverName,
+      content: this.serverContent,
+    });
+  }
+}
