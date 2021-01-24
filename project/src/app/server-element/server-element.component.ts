db8daba3dfcb676a025dc893ee0af5ebf79d0012
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-01-24 09:33:19 +0100

    @Input

--- project/src/app/server-element/server-element.component.ts
+++ project/src/app/server-element/server-element.component.ts
@@ -5,6 +5,10 @@ import { Component, Input } from '@angular/core';
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.scss'],
 })
+
+//Pouzivame @Input dekorator, pretoze data nam prichadzaju z rodicovskeho elementu z app.component.ts, nasledne su tieto data poslane do app.component.html do nasej komponenty <server-element></server-element> cez atribut [serverElement]. Tento atribut ma hodnotu rovnu jednemu prvku z pola serverElements. Tento prvok bude ocakavat 3 premenne (type, name a content)
 export class ServerElementComponent {
   @Input() serverElement: { type: string; name: string; content: string };
 }
+
+// https://blog.hackages.io/angular-component-interaction-with-input-output-and-eventemitter-72526422b95c
