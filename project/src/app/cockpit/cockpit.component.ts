c1ef5d6123aa8f7f896c12bd3aef022c0ddb350f
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-01-24 09:32:05 +0100

    @Output, EventEmitter(), emit()

--- project/src/app/cockpit/cockpit.component.ts
+++ project/src/app/cockpit/cockpit.component.ts
@@ -6,10 +6,13 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./cockpit.component.scss'],
 })
 export class CockpitComponent {
+  //Tieto premenne sa zasielaju do [(ngModel)] v cockpit.component.html subore
   serverName = '';
   serverContent = '';
 
-  //Kedze mame dve funkcie, ktore po kliknuti na button maju prepisovat stav v premennych, musime si vytvorit instanciu EventEmitter(). Ta sa pouziva na vytvorenie vazby funkcie s udalostou, emit sa pouziva na spustenie udalosti. Vyuzivame @Output dekorator, kedze data posielame smerom von z komponenty
+  //Kedze mame dve funkcie, ktore po kliknuti na button maju prepisovat hodnotu v premennych serverName a serverContent, musime si vytvorit instanciu EventEmitter(). Ta sa pouziva na vytvorenie vazby funkcie s udalostou, emit() sa pouziva na spustenie udalosti. Vyuzivame @Output dekorator, kedze data posielame smerom von z komponenty do rodicovskej komponenty(createGreenServers a createYellowServers su preposlane do app.component.html)
+
+  //premenne v objekte, ktory vchadza do EventEmitteru musia byt pomenovane rovnako ako premenne v objekte data v addGreenServers a addYellowServers v subore cockpit.components.ts
   @Output() createGreenServers = new EventEmitter<{
     name: string;
     content: string;
